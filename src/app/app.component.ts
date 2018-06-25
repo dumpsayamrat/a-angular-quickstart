d3e057ad0410049d000db2563fead4e4a1915d7b
Kaewta <Sayamrat.Kaewta@thomsonreuters.com>
2018-06-25 21:23:56 +0700

    init angular quickstart

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+    <h1>Todo List</h1>
+    <ul>
+      <li>Do Something</li>
+    </ul>
+  `
+})
+export class AppComponent {
+}
